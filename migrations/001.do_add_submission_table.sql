a1ebf97d34f3c166b4de57adfda9077cf440bd08
jamesjenkinsjr <jamesjenkinsjr@icloud.com>
2019-11-25 15:37:26 -0500

    add migration to create submission table

---
+++ migrations/001.do_add_submission_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE "submission" (
+  "id" SERIAL PRIMARY KEY,
+  "image_url" TEXT NOT NULL,
+  "karma_total" INTEGER DEFAULT 0,
+  "latitude" TEXT NOT NULL,
+  "longitude" TEXT NOT NULL,
+  "create_timestamp" TIMESTAMP DEFAULT NOW()
+);
