75f9478a833f8ae76c7db003a58b8450b213adad
Kevin Wei <chenghaowei@gmail.com>
2019-11-26 10:21:00 -0500

    remove unneeded jsonParse from post endpoint, remove uploads folder, pass error to next in post endpoint

--- src/routers/images-router.js
+++ src/routers/images-router.js
@@ -2,21 +2,20 @@ const express = require('express');
 const multer = require('multer');
 const UploadService = require('../services/upload-service');
 const imagesRouter = express.Router();
-const jsonParser = express.json();
 
 const UPLOAD_PATH = 'uploads/';
 const upload = multer({ dest: `${UPLOAD_PATH}` });
 
 imagesRouter
   .route('/')
-  .post(jsonParser, upload.single('someImage'), async (req, res, next) => {
+  .post(upload.single('someImage'), async (req, res, next) => {
     try {
       console.log(req.file);
       const { path, filename, mimetype } = req.file;
       await UploadService.uploadFile(path, filename, mimetype);
       return res.redirect('/');
     } catch (error) {
-      return res.status(400);
+      next(error);
     }
   });
 
