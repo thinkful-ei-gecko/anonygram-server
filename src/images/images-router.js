2426f55a2dc1f1b6d45125a888d153cf783431f2
Kevin Wei <chenghaowei@gmail.com>
2019-11-26 14:06:01 -0500

    parse latitude/longitude from request body

--- src/images/images-router.js
+++ src/images/images-router.js
@@ -1,28 +1,31 @@
 const express = require('express');
 const multer = require('multer');
-const { uploadFile } = require('../utils/upload-util');
+const jsonParser = express.json();
 const imagesRouter = express.Router();
 const ImagesService = require('./images-service');
+const { uploadFile } = require('../utils/upload-util');
 
 const UPLOAD_PATH = 'uploads/';
 const upload = multer({ dest: `${UPLOAD_PATH}` });
 
-imagesRouter.route('/').post(upload.single('someImage'), async (req, res, next) => {
-  try {
-    console.log(req.file);
-    const { path, filename, mimetype } = req.file;
-    const image_url = await uploadFile(path, filename, mimetype);
-    const newSubmission = await ImagesService.createSubmission(req.app.get('db'), {
-      image_url,
-      karma_total: 0,
-      latitude: '',
-      longitude: '',
-    });
+imagesRouter
+  .route('/')
+  .post(jsonParser, upload.single('someImage'), async (req, res, next) => {
+    try {
+      console.log(req.file);
+      const { latitude, longitude } = req.body;
+      const { path, filename, mimetype } = req.file;
+      const image_url = await uploadFile(path, filename, mimetype);
+      const newSubmission = await ImagesService.createSubmission(req.app.get('db'), {
+        image_url,
+        latitude,
+        longitude,
+      });
 
-    return res.status(201).json(newSubmission);
-  } catch (error) {
-    next(error);
-  }
-});
+      return res.status(201).json(newSubmission);
+    } catch (error) {
+      next(error);
+    }
+  });
 
 module.exports = imagesRouter;
