ee6babe198e740cb3a7cd4fb0cfd9b12c87f9466
Kevin Wei <chenghaowei@gmail.com>
2019-11-27 10:29:04 -0500

    dynamic origin based on node environment

--- src/config.js
+++ src/config.js
@@ -1,8 +1,12 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
-  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/anonygram',
+  CLIENT_ORIGIN:
+    process.env.NODE_ENV === 'production'
+      ? process.env.CLIENT_ORIGIN
+      : 'http://localhost:3000',
+  DATABASE_URL:
+    process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/anonygram',
   AWS_ID: process.env.AWS_ID,
   AWS_SECRET: process.env.AWS_SECRET,
   AWS_BUCKET: process.env.AWS_BUCKET,
