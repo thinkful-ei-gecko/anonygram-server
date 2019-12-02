8fdabea10e82c2778778604b5c296d8f2fb8ca9f
jamesjenkinsjr <jamesjenkinsjr@icloud.com>
2019-12-02 16:00:25 -0500

    add migration files and seed data for users table, install bcryptjs

--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -1,6 +1,7 @@
 BEGIN;
 
 TRUNCATE "submission";
+TRUNCATE "users";
 
 
 INSERT INTO "submission" ("id", "image_url", "karma_total", "latitude", "longitude", "create_timestamp")
@@ -47,5 +48,12 @@ VALUES ( 1,
 -- because we explicitly set the id fields
 -- update the sequencer for future automatic id setting
 SELECT setval('submission_id_seq', (SELECT MAX(id) from "submission"));
+
+INSERT INTO users ("username", "password")
+ VALUES (
+   'admin', 
+   -- password
+   '$2a$12$LYtNBy8xFQfDwU1CuyrCrO9UIOlxlosDiSZDnsCtvAyRLolHwaChe'
+   );
 
 COMMIT;
