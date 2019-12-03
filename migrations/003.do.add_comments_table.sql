9eba291f49293fbe5e4f5f1cfb6137be4ebbffcc
Kevin Wei <chenghaowei@gmail.com>
2019-12-03 16:46:04 -0500

    allow null for comment timestamp, seed test comment

--- migrations/003.do.add_comments_table.sql
+++ migrations/003.do.add_comments_table.sql
@@ -1,9 +1,7 @@
 CREATE TABLE comments (
     comment_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
     comment_text TEXT NOT NULL,
-    comment_timestamp TIMESTAMP DEFAULT now() NOT NULL,
-    submission_id INTEGER
-        REFERENCES submission(id) ON DELETE CASCADE NOT NULL,
-    user_id uuid
-        REFERENCES users(id) ON DELETE CASCADE NOT NULL
+    comment_timestamp TIMESTAMP DEFAULT now(),
+    submission_id INTEGER REFERENCES submission(id) ON DELETE CASCADE NOT NULL,
+    user_id uuid REFERENCES users(id) ON DELETE CASCADE NOT NULL
 );
