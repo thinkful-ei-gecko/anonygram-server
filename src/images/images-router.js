927721ecb5616ea4ce90cd1a1637d90354f41538
jamesjenkinsjr <jamesjenkinsjr@icloud.com>
2019-12-04 12:28:15 -0500

    return of the semi

--- src/images/images-router.js
+++ src/images/images-router.js
@@ -1,45 +1,45 @@
-const express = require('express')
-const jsonParser = express.json()
-const multer = require('multer')
+const express = require('express');
+const jsonParser = express.json();
+const multer = require('multer');
 const {
   uploadFile,
   removeFile,
   acceptImagesOnly,
-} = require('../utils/file-util')
-const { getDistanceFromLatLonInKm } = require('../utils/location-util')
-const { checkNSFWLikely } = require('../utils/vision-util')
+} = require('../utils/file-util');
+const { getDistanceFromLatLonInKm } = require('../utils/location-util');
+const { checkNSFWLikely } = require('../utils/vision-util');
 const {
   getDefaultPlaceData,
   getImageByReference,
-} = require('../utils/places-util')
-const imagesRouter = express.Router()
-const ImagesService = require('./images-service')
-const upload = multer({ dest: 'uploads/', fileFilter: acceptImagesOnly })
-const sharp = require('sharp')
+} = require('../utils/places-util');
+const imagesRouter = express.Router();
+const ImagesService = require('./images-service');
+const upload = multer({ dest: 'uploads/', fileFilter: acceptImagesOnly });
+const sharp = require('sharp');
 
 imagesRouter
   .route('/')
   .get(async (req, res, next) => {
-    const { sort, lat, lon, page } = req.query
+    const { sort, lat, lon, page } = req.query;
 
     if (sort !== 'top' && sort !== 'new') {
       return res
         .status(400)
-        .json({ error: 'Invalid value provided for sort param' })
+        .json({ error: 'Invalid value provided for sort param' });
     }
 
     if (!lat || !lon) {
       return res
         .status(400)
-        .json({ error: 'lat and lon parameters are required' })
+        .json({ error: 'lat and lon parameters are required' });
     } else if (!parseFloat(lat) || !parseFloat(lon)) {
       return res
         .status(400)
-        .json({ error: 'lat and lon parameters are invalid' })
+        .json({ error: 'lat and lon parameters are invalid' });
     }
 
     if (!!page && !parseInt(page)) {
-      return res.status(400).json({ error: 'page parameter is invalid' })
+      return res.status(400).json({ error: 'page parameter is invalid' });
     }
 
     try {
@@ -50,9 +50,9 @@ imagesRouter
         req.app.get('db'),
         sort,
         page || null
-      )
+      );
 
-      const submissionsByLocation = []
+      const submissionsByLocation = [];
       // filter all submissions for their distance against the request lat/lon
       if (!!submissions.length) {
         submissions.forEach(submission => {
@@ -61,13 +61,13 @@ imagesRouter
             lon,
             submission['latitude'],
             submission['longitude']
-          )
+          );
 
           // bundle all posts that are within ~20km of the req lat/long
           if (distance < 20) {
-            return submissionsByLocation.push(submission)
+            return submissionsByLocation.push(submission);
           }
-        })
+        });
       }
 
       // if we do not have any data, we are using Google to make some
@@ -76,9 +76,12 @@ imagesRouter
       // pagination can technically otherwise cause it to seem like
       // no data artificially
 
-      if (!submissionsByLocation.length && (!page || (!!page && parseInt(page) === 1))) {
+      if (
+        !submissionsByLocation.length &&
+        (!page || (!!page && parseInt(page) === 1))
+      ) {
         // make a request to get place coordinates and the photo_reference
-        let defaultPlaces = await getDefaultPlaceData(lat, lon)
+        let defaultPlaces = await getDefaultPlaceData(lat, lon);
         await Promise.all(
           defaultPlaces.results.map(async location => {
             // if the location has a photos attribute, grab the reference and
@@ -87,65 +90,65 @@ imagesRouter
             if (location.photos) {
               let image_url = await getImageByReference(
                 location.photos[0]['photo_reference']
-              )
+              );
               const submission = {
                 image_url,
                 latitude: location.geometry.location.lat,
                 longitude: location.geometry.location.lng,
-              }
+              };
 
               // insert these new submissions into the database
               await ImagesService.createSubmission(
                 req.app.get('db'),
                 submission
-              )
+              );
             }
           })
-        )
+        );
         const googleSubmissions = await ImagesService.getSubmissions(
           req.app.get('db'),
           sort
-        )
+        );
 
         // if somehow even Google has no nearby locations with images
         // send an error
         if (!googleSubmissions.length) {
-          return res.status(400).json({ error: 'no submissions available' })
+          return res.status(400).json({ error: 'no submissions available' });
         }
-        return res.status(200).json(googleSubmissions)
+        return res.status(200).json(googleSubmissions);
       }
 
-      return res.status(200).json(submissionsByLocation)
+      return res.status(200).json(submissionsByLocation);
     } catch (e) {
-      next(e)
+      next(e);
     }
   })
   .post(jsonParser, upload.single('someImage'), async (req, res, next) => {
     try {
-      console.log(req.file)
-      const { image_text, latitude, longitude } = req.body
-      const { path, filename } = req.file
-      const isNSFW = await checkNSFWLikely(path)
+      console.log(req.file);
+      const { image_text, latitude, longitude } = req.body;
+      const { path, filename } = req.file;
+      const isNSFW = await checkNSFWLikely(path);
 
       if (isNSFW) {
-        removeFile(path) // remove uploaded file from disk
+        removeFile(path); // remove uploaded file from disk
         return res.status(400).json({
           error: 'provided content does not meet community guidelines',
-        })
+        });
       }
 
       const imageData = await sharp(path)
         .rotate() // auto-rotate based on EXIF metadata
         .resize({ width: 1200, withoutEnlargement: true }) // limit max width
         .jpeg({ quality: 70 }) // compress to jpeg with indistinguishable quality difference
-        .toBuffer() // returns a Promise<Buffer>
+        .toBuffer(); // returns a Promise<Buffer>
 
       const image_url = await uploadFile(
         imageData,
         path,
         filename,
         'image/jpeg'
-      )
+      );
       const newSubmission = await ImagesService.createSubmission(
         req.app.get('db'),
         {
@@ -154,53 +157,53 @@ imagesRouter
           latitude,
           longitude,
         }
-      )
+      );
 
-      return res.status(201).json(newSubmission)
+      return res.status(201).json(newSubmission);
     } catch (error) {
-      next(error)
+      next(error);
     }
-  })
+  });
 
 imagesRouter
   .use(jsonParser)
   .route('/:submission_id')
   .all(async (req, res, next) => {
-    const id = req.params.submission_id
+    const id = req.params.submission_id;
     const submission = await ImagesService.getSingleSubmission(
       req.app.get('db'),
       id
-    )
+    );
 
     if (!submission) {
-      return res.status(400).json({ error: 'id does not exist' })
+      return res.status(400).json({ error: 'id does not exist' });
     }
 
-    res.submission = submission
-    next()
+    res.submission = submission;
+    next();
   })
   .patch(async (req, res, next) => {
-    const { karma_total } = req.body
+    const { karma_total } = req.body;
 
     if (!karma_total) {
-      return res.status(400).json({ error: 'karma_total is required' })
+      return res.status(400).json({ error: 'karma_total is required' });
     }
 
     try {
       const submissionData = {
         ...res.submission,
         karma_total,
-      }
+      };
 
       const updatedSubmission = await ImagesService.updateSingleSubmission(
         req.app.get('db'),
         res.submission.id,
         submissionData
-      )
-      return res.status(200).json(updatedSubmission)
+      );
+      return res.status(200).json(updatedSubmission);
     } catch (e) {
-      next(e)
+      next(e);
     }
-  })
+  });
 
-module.exports = imagesRouter
+module.exports = imagesRouter;
--- src/images/images-service.js
+++ src/images/images-service.js
@@ -1,49 +1,48 @@
 const ImagesService = {
   getSubmissions: (db, sort = 'new', page = null) => {
-    const PAGINATION_VALUE = 10
-    const pageNum = parseInt(page) - 1
+    const PAGINATION_VALUE = 10;
+    const pageNum = parseInt(page) - 1;
     if (!page) {
       if (sort === 'new') {
         return db('submission')
           .select('*')
-          .orderBy('create_timestamp', 'DESC')
+          .orderBy('create_timestamp', 'DESC');
       } else {
         return db('submission')
           .select('*')
-          .orderBy('karma_total', 'DESC')
+          .orderBy('karma_total', 'DESC');
       }
     } else {
-      console.log(PAGINATION_VALUE * pageNum)
+      console.log(PAGINATION_VALUE * pageNum);
       if (sort === 'new') {
         return db('submission')
           .select('*')
           .limit(PAGINATION_VALUE)
           .offset(PAGINATION_VALUE * pageNum)
-          .orderBy('create_timestamp', 'DESC')
+          .orderBy('create_timestamp', 'DESC');
       } else {
         return db('submission')
           .select('*')
           .limit(PAGINATION_VALUE)
           .offset(PAGINATION_VALUE * pageNum)
-          .orderBy('karma_total', 'DESC')
+          .orderBy('karma_total', 'DESC');
       }
     }
-      
   },
 
   getSingleSubmission: (db, id) => {
     return db('submission')
       .select('*')
       .where({ id })
-      .first()
+      .first();
   },
 
   updateSingleSubmission: (db, id, data) => {
-    const karma_total = data.karma_total 
+    const karma_total = data.karma_total;
     return db('submission')
-      .where({'id': id })
-      .update({'karma_total': karma_total})
-      .then(() => ImagesService.getSingleSubmission(db, id))
+      .where({ id: id })
+      .update({ karma_total: karma_total })
+      .then(() => ImagesService.getSingleSubmission(db, id));
   },
 
   createSubmission(db, submission) {
@@ -51,7 +50,7 @@ const ImagesService = {
       .insert(submission)
       .into('submission')
       .returning('*')
-      .then((rows) => {
+      .then(rows => {
         return rows[0];
       });
   },
