2d90707841ab23a8dcd54d54a0a21e51e6194812
Kevin Wei <chenghaowei@gmail.com>
2019-12-04 14:42:19 -0500

    add delete endpoint (remove db row and s3 object)

--- src/images/images-router.js
+++ src/images/images-router.js
@@ -2,16 +2,14 @@ const express = require('express');
 const jsonParser = express.json();
 const multer = require('multer');
 const {
-  uploadFile,
-  removeFile,
+  uploadToS3,
+  removeFromS3,
+  removeFromDisk,
   acceptImagesOnly,
 } = require('../utils/file-util');
 const { getDistanceFromLatLonInKm } = require('../utils/location-util');
 const { checkNSFWLikely } = require('../utils/vision-util');
-const {
-  getDefaultPlaceData,
-  getImageByReference,
-} = require('../utils/places-util');
+const { getDefaultPlaceData, getImageByReference } = require('../utils/places-util');
 const imagesRouter = express.Router();
 const ImagesService = require('./images-service');
 const upload = multer({ dest: 'uploads/', fileFilter: acceptImagesOnly });
@@ -23,19 +21,13 @@ imagesRouter
     const { sort, lat, lon, page } = req.query;
 
     if (sort !== 'top' && sort !== 'new') {
-      return res
-        .status(400)
-        .json({ error: 'Invalid value provided for sort param' });
+      return res.status(400).json({ error: 'Invalid value provided for sort param' });
     }
 
     if (!lat || !lon) {
-      return res
-        .status(400)
-        .json({ error: 'lat and lon parameters are required' });
+      return res.status(400).json({ error: 'lat and lon parameters are required' });
     } else if (!parseFloat(lat) || !parseFloat(lon)) {
-      return res
-        .status(400)
-        .json({ error: 'lat and lon parameters are invalid' });
+      return res.status(400).json({ error: 'lat and lon parameters are invalid' });
     }
 
     if (!!page && !parseInt(page)) {
@@ -55,7 +47,7 @@ imagesRouter
       const submissionsByLocation = [];
       // filter all submissions for their distance against the request lat/lon
       if (!!submissions.length) {
-        submissions.forEach(submission => {
+        submissions.forEach((submission) => {
           let distance = getDistanceFromLatLonInKm(
             lat,
             lon,
@@ -76,14 +68,11 @@ imagesRouter
       // pagination can technically otherwise cause it to seem like
       // no data artificially
 
-      if (
-        !submissionsByLocation.length &&
-        (!page || (!!page && parseInt(page) === 1))
-      ) {
+      if (!submissionsByLocation.length && (!page || (!!page && parseInt(page) === 1))) {
         // make a request to get place coordinates and the photo_reference
         let defaultPlaces = await getDefaultPlaceData(lat, lon);
         await Promise.all(
-          defaultPlaces.results.map(async location => {
+          defaultPlaces.results.map(async (location) => {
             // if the location has a photos attribute, grab the reference and
             // make a separate request to retrieve the hosted image URL from
             // the reference ID
@@ -98,10 +87,7 @@ imagesRouter
               };
 
               // insert these new submissions into the database
-              await ImagesService.createSubmission(
-                req.app.get('db'),
-                submission
-              );
+              await ImagesService.createSubmission(req.app.get('db'), submission);
             }
           })
         );
@@ -133,15 +119,15 @@ imagesRouter
       if (!latitude || !longitude) {
         return res
           .status(400)
-          .json({ error: 'latitude and longitude parameters are required' })
+          .json({ error: 'latitude and longitude parameters are required' });
       } else if (!parseFloat(latitude) || !parseFloat(longitude)) {
         return res
           .status(400)
-          .json({ error: 'latitude and longitude parameters are invalid' })
+          .json({ error: 'latitude and longitude parameters are invalid' });
       }
 
       if (isNSFW) {
-        removeFile(path); // remove uploaded file from disk
+        removeFromDisk(path); // remove uploaded file from disk
         return res.status(400).json({
           error: 'provided content does not meet community guidelines',
         });
@@ -150,12 +136,12 @@ imagesRouter
       // we need to obfuscate coordinates so that users in homes or
       // private places are not easily identified
 
-      const latArr = latitude.split('.')
-      const lonArr = longitude.split('.')
-      latArr[1] = latArr[1].substring(0, 3)
-      lonArr[1] = lonArr[1].substring(0, 3)
-      latitude = latArr.join('.')
-      longitude = lonArr.join('.')
+      const latArr = latitude.split('.');
+      const lonArr = longitude.split('.');
+      latArr[1] = latArr[1].substring(0, 3);
+      lonArr[1] = lonArr[1].substring(0, 3);
+      latitude = latArr.join('.');
+      longitude = lonArr.join('.');
 
       const imageData = await sharp(path)
         .rotate() // auto-rotate based on EXIF metadata
@@ -163,21 +149,13 @@ imagesRouter
         .jpeg({ quality: 70 }) // compress to jpeg with indistinguishable quality difference
         .toBuffer(); // returns a Promise<Buffer>
 
-      const image_url = await uploadFile(
-        imageData,
-        path,
-        filename,
-        'image/jpeg'
-      );
-      const newSubmission = await ImagesService.createSubmission(
-        req.app.get('db'),
-        {
-          image_url,
-          image_text,
-          latitude,
-          longitude,
-        }
-      );
+      const image_url = await uploadToS3(imageData, path, filename, 'image/jpeg');
+      const newSubmission = await ImagesService.createSubmission(req.app.get('db'), {
+        image_url,
+        image_text,
+        latitude,
+        longitude,
+      });
 
       return res.status(201).json(newSubmission);
     } catch (error) {
@@ -190,10 +168,7 @@ imagesRouter
   .route('/:submission_id')
   .all(async (req, res, next) => {
     const id = req.params.submission_id;
-    const submission = await ImagesService.getSingleSubmission(
-      req.app.get('db'),
-      id
-    );
+    const submission = await ImagesService.getSingleSubmission(req.app.get('db'), id);
 
     if (!submission) {
       return res.status(400).json({ error: 'id does not exist' });
@@ -223,6 +198,17 @@ imagesRouter
       return res.status(200).json(updatedSubmission);
     } catch (e) {
       next(e);
+    }
+  })
+  .delete(async (req, res, next) => {
+    try {
+      const url = res.submission.image_url;
+      const s3ObjectKey = url.substring(url.lastIndexOf('/') + 1);
+      await ImagesService.deleteSubmission(req.app.get('db'), res.submission.id);
+      await removeFromS3(s3ObjectKey);
+      return res.status(204).end();
+    } catch (error) {
+      next(error);
     }
   });
 
