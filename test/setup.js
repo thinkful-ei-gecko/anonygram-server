c944c0942ed61406b5fe3377df1340276ea25666
Kevin Wei <chenghaowei@gmail.com>
2019-12-06 17:40:53 -0500

    add chai-fs assertion library for testing uploads

--- test/setup.js
+++ test/setup.js
@@ -3,6 +3,7 @@ process.env.JWT_SECRET = 'test-jwt-secret';
 
 require('dotenv').config();
 const chai = require('chai');
+chai.use(require('chai-fs'));
 const supertest = require('supertest');
 
 global.chai = chai;
