3949d25334da16826db1e86d99de4622eb7a5fef
Kevin Wei <chenghaowei@gmail.com>
2019-12-09 12:18:31 -0500

    add karma_balance field to users, debit karma balance when upvoting

--- migrations/002.do.add_user_table.sql
+++ migrations/002.do.add_user_table.sql
@@ -4,5 +4,6 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE users (
   id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
   username text NOT NULL,
-  password text NOT NULL
+  password text NOT NULL,
+  karma_balance INTEGER DEFAULT 50,
 );
